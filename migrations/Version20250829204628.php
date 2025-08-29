0dccb5275941415bdfd7e9674d9d03bea925a78a
I851331 <fernando.frezza@sap.com>
2025-08-29 17:07:30 -0400

    Add news category entity and form

---
+++ migrations/Version20250829204628.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250829204628 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE news_category ADD COLUMN title VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__product_property AS SELECT id, title FROM product_property');
+        $this->addSql('DROP TABLE product_property');
+        $this->addSql('CREATE TABLE product_property (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) DEFAULT NULL)');
+        $this->addSql('INSERT INTO product_property (id, title) SELECT id, title FROM __temp__product_property');
+        $this->addSql('DROP TABLE __temp__product_property');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__news_category AS SELECT id FROM news_category');
+        $this->addSql('DROP TABLE news_category');
+        $this->addSql('CREATE TABLE news_category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)');
+        $this->addSql('INSERT INTO news_category (id) SELECT id FROM __temp__news_category');
+        $this->addSql('DROP TABLE __temp__news_category');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__product_property AS SELECT id, title FROM product_property');
+        $this->addSql('DROP TABLE product_property');
+        $this->addSql('CREATE TABLE product_property (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL)');
+        $this->addSql('INSERT INTO product_property (id, title) SELECT id, title FROM __temp__product_property');
+        $this->addSql('DROP TABLE __temp__product_property');
+    }
+}
--- src/Entity/NewsCategory.php
+++ src/Entity/NewsCategory.php
@@ -2,18 +2,18 @@
 
 namespace App\Entity;
 
-use App\Repository\ProductPropertyRepository;
+use App\Repository\NewsCategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: ProductPropertyRepository::class)]
-class ProductProperty
+#[ORM\Entity(repositoryClass: NewsCategoryRepository::class)]
+class NewsCategory
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(length: 255)]
     private ?string $title = null;
 
     public function getId(): ?int
