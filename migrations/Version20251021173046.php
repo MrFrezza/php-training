7b3d5ae593d7f3f402dbdabb062ade2612fd224a
I851331 <fernando.frezza@sap.com>
2025-10-21 15:59:56 -0400

    Primeira part CRUD do banner

---
+++ migrations/Version20251021173046.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251021173046 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE banner (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) DEFAULT NULL, subtitle VARCHAR(255) DEFAULT NULL, active INTEGER DEFAULT NULL, position INTEGER DEFAULT NULL, url VARCHAR(255) DEFAULT NULL, image VARCHAR(255) DEFAULT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE banner');
+    }
+}
---
+++ src/Form/BannerType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Banner;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BannerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('subtitle')
+            ->add('active', ChoiceType::class, [
+                'choices' => [
+                    'Ativo' => 1,
+                    'Inativo' => 0
+                ]
+            ])
+            ->add('position')
+            ->add('url')
+            ->add('image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Banner::class,
+        ]);
+    }
+}
